3616733f081bd0bdf9029ac13accb5fff37365b8
Johan Ramirez <johand200505@gmail.com>
2026-06-13 15:03:26 -0400

    Add formatting

--- deli.py
+++ deli.py
@@ -3,5 +3,11 @@ finished_sandwiches = []
 
 while sandwich_orders:
     current = sandwich_orders.pop()
-    print(f"Made your {current.title()} sandwich.")
+    print(f"I have made your {current.title()} sandwich.")
     finished_sandwiches.append(current)
+
+print(f"These are the sandwiches that I made: ", end="")
+while finished_sandwiches:
+    current = finished_sandwiches.pop(0)
+    print(f"{current.title()} " end="")
+
