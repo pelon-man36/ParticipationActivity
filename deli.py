52a5034ffb8bfbc8501366f50786c78b20327b40
Johan Ramirez <johand200505@gmail.com>
2026-06-13 14:53:11 -0400

    Add while loop that loops through orders, then places them in finished

--- deli.py
+++ deli.py
@@ -1,2 +1,7 @@
 sandwich_orders = ["tuna", "turkey", "ham", "veggie", "cheese"]
 finished_sandwiches = []
+
+while sandwich_orders:
+    current = sandwich_orders.pop()
+    print(f"Made your {current.title()} sandwich.")
+    finished_sandwiches.append(current)
