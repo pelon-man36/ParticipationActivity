559bf88239550db84afb1befb49ccde5aaeb39b7
Johan Ramirez <johand200505@gmail.com>
2026-06-13 15:10:27 -0400

    Removed formatting

--- deli.py
+++ deli.py
@@ -6,8 +6,7 @@ while sandwich_orders:
     print(f"I have made your {current.title()} sandwich.")
     finished_sandwiches.append(current)
 
-print(f"These are the sandwiches that I made: ", end="")
 while finished_sandwiches:
     current = finished_sandwiches.pop(0)
-    print(f"{current.title()} " end="")
+    
 
