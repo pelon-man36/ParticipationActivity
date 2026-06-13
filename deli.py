1a6ec8fd6dcfcfdec5fb4327f7b184fe55548cd9
Johan Ramirez <johand200505@gmail.com>
2026-06-13 15:14:52 -0400

    Add fixed formatting

--- deli.py
+++ deli.py
@@ -6,7 +6,9 @@ while sandwich_orders:
     print(f"I have made your {current.title()} sandwich.")
     finished_sandwiches.append(current)
 
+print("These are all the sandwiches I made:")
 while finished_sandwiches:
     current = finished_sandwiches.pop(0)
-    
+    print(f"{current.title()}", end=", ")
 
+print("so have a nice day!")
